318e50b50819688126f3128d94bc9d30e1a290de
Igor Rezende <sr.igor.dev@gmail.com>
2026-01-29 16:42:28 -0300

    WIP: remove erro from dom with skip html form

--- packages/form/src/presentation/components/fields/switch/switch.component.tsx
+++ packages/form/src/presentation/components/fields/switch/switch.component.tsx
@@ -1,44 +1,50 @@
-'use client';
+"use client";
 
-import { Controller, FieldValues } from 'react-hook-form';
+import { Controller, FieldValues } from "react-hook-form";
 
-import { cn } from '@repo/ui';
-import { StatusCircle } from '@repo/ui';
+import { cn, StatusCircle } from "@repo/ui";
 
-import { IFieldComponentProps } from '../../../../core/domain/interfaces';
-import { FieldContainer } from '../../base/container/field-container.component';
-import { Switch } from './switch-export.component';
+import { IFieldComponentProps } from "../../../../core/domain/interfaces";
+import { FieldContainer } from "../../base/container/field-container.component";
+import { Switch } from "./switch-export.component";
 
 export function SwitchField<FormType extends FieldValues>({
-    name,
-    control,
-    className,
-    inputClassName,
-    read,
-    ...rest
+  name,
+  control,
+  className,
+  inputClassName,
+  read,
+  skipHtmlFor,
+  ...rest
 }: IFieldComponentProps<FormType> & React.DetailedHTMLProps<any, any>) {
-    const isOnlyRead = read === 'true';
+  const isOnlyRead = read === "true";
 
-    return (
-        <Controller
-            control={control}
-            name={name}
-            render={({ field, fieldState }) => (
-                <FieldContainer className={className} name={name} error={fieldState.error?.message} {...rest}>
-                    {!isOnlyRead && (
-                        <Switch
-                            checked={field.value}
-                            {...rest}
-                            required={false}
-                            tabIndex={-1}
-                            className={cn('text-foreground h-8 w-16', inputClassName, 'rounded-full border-none')}
-                            onCheckedChange={field.onChange as (value: boolean) => void}
-                        />
-                    )}
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState }) => (
+        <FieldContainer
+          className={className}
+          name={name}
+          error={fieldState.error?.message}
+          skipHtmlFor={skipHtmlFor}
+          {...rest}
+        >
+          {!isOnlyRead && (
+            <Switch
+              checked={field.value}
+              {...rest}
+              required={false}
+              tabIndex={-1}
+              className={cn("text-foreground h-8 w-16", inputClassName, "rounded-full border-none")}
+              onCheckedChange={field.onChange as (value: boolean) => void}
+            />
+          )}
 
-                    {isOnlyRead && <StatusCircle status={field.value} className="h-8 w-8" />}
-                </FieldContainer>
-            )}
-        />
-    );
+          {isOnlyRead && <StatusCircle status={field.value} className="h-8 w-8" />}
+        </FieldContainer>
+      )}
+    />
+  );
 }
