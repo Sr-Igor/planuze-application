ab40a31eeaa4270b835047661a4293235d1d1bf6
Igor Rezende <sr.igor.dev@gmail.com>
2026-01-31 18:19:54 -0300

    WIP: add notification sound in service worker

--- apps/web/public/sw.js
+++ apps/web/public/sw.js
@@ -1,95 +1,99 @@
-self.addEventListener('push', function (event) {
-    const data = event.data.json();
-    const { title, body, icon } = data.notification;
+self.addEventListener("push", function (event) {
+  const data = event.data.json();
+  const { title, body, icon, sound } = data.notification;
 
-    const options = {
-        body: body,
-        icon: icon,
-        badge: icon,
-        data: data.data || {}
-    };
+  const options = {
+    body: body,
+    icon: icon,
+    badge: icon,
+    vibrate: [200, 100, 200],
+    sound: sound || "/notification.mp3",
+    data: data.data || {},
+  };
 
-    event.waitUntil(self.registration.showNotification(title, options));
+  event.waitUntil(self.registration.showNotification(title, options));
 });
 
-self.addEventListener('notificationclick', function (event) {
-    event.notification.close();
+self.addEventListener("notificationclick", function (event) {
+  event.notification.close();
 
-    let redirect = '/';
-    let searchParams = null;
-    try {
-        const notificationData = event.notification.data;
-        redirect = notificationData?.redirect || '/';
-        searchParams = notificationData?.searchParams || null;
-    } catch (e) {
-        //Fallback
-    }
+  let redirect = "/";
+  let searchParams = null;
+  try {
+    const notificationData = event.notification.data;
+    redirect = notificationData?.redirect || "/";
+    searchParams = notificationData?.searchParams || null;
+  } catch (e) {
+    console.error(e);
+    //Fallback
+  }
 
-    event.waitUntil(
-        clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
-            const base = self.location.origin;
+  event.waitUntil(
+    clients.matchAll({ type: "window", includeUncontrolled: true }).then(function (clientList) {
+      const base = self.location.origin;
 
-            let matchingClient = null;
+      let matchingClient = null;
 
-            for (let i = 0; i < clientList.length; i++) {
-                const client = clientList[i];
+      for (let i = 0; i < clientList.length; i++) {
+        const client = clientList[i];
 
-                if (client.url === base + redirect) {
-                    matchingClient = client;
-                    break;
-                }
-            }
+        if (client.url === base + redirect) {
+          matchingClient = client;
+          break;
+        }
+      }
 
-            if (!matchingClient) {
-                for (let i = 0; i < clientList.length; i++) {
-                    const client = clientList[i];
+      if (!matchingClient) {
+        for (let i = 0; i < clientList.length; i++) {
+          const client = clientList[i];
 
-                    if (client.visibilityState === 'visible') {
-                        matchingClient = client;
-                        break;
-                    }
-                }
-            }
+          if (client.visibilityState === "visible") {
+            matchingClient = client;
+            break;
+          }
+        }
+      }
 
-            function addSearchParamsToUrl(url, paramsObj) {
-                try {
-                    const urlObj = new URL(url, self.location.origin);
-                    Object.entries(paramsObj).forEach(([key, value]) => {
-                        urlObj.searchParams.set(key, value);
-                    });
-                    return urlObj.toString();
-                } catch (e) {
-                    return url;
-                }
-            }
+      function addSearchParamsToUrl(url, paramsObj) {
+        try {
+          const urlObj = new URL(url, self.location.origin);
+          Object.entries(paramsObj).forEach(([key, value]) => {
+            urlObj.searchParams.set(key, value);
+          });
+          return urlObj.toString();
+        } catch (e) {
+          console.error(e);
+          return url;
+        }
+      }
 
-            if (matchingClient) {
-                let finalUrl = base + redirect;
-                if (searchParams) {
-                    finalUrl = addSearchParamsToUrl(matchingClient.url, searchParams);
-                }
-                let focused = false;
-                if ('focus' in matchingClient) {
-                    matchingClient.focus();
-                    focused = true;
-                }
-                if ('navigate' in matchingClient) {
-                    return matchingClient.navigate(finalUrl);
-                }
-                if (!focused || !('navigate' in matchingClient)) {
-                    if (clients.openWindow) {
-                        return clients.openWindow(finalUrl);
-                    }
-                }
-                return;
-            }
+      if (matchingClient) {
+        let finalUrl = base + redirect;
+        if (searchParams) {
+          finalUrl = addSearchParamsToUrl(matchingClient.url, searchParams);
+        }
+        let focused = false;
+        if ("focus" in matchingClient) {
+          matchingClient.focus();
+          focused = true;
+        }
+        if ("navigate" in matchingClient) {
+          return matchingClient.navigate(finalUrl);
+        }
+        if (!focused || !("navigate" in matchingClient)) {
+          if (clients.openWindow) {
+            return clients.openWindow(finalUrl);
+          }
+        }
+        return;
+      }
 
-            let finalUrl = base + redirect;
-            if (searchParams) {
-                finalUrl = addSearchParamsToUrl(base, searchParams);
-            }
-            if (clients.openWindow) return clients.openWindow(finalUrl);
-            return;
-        })
-    );
+      let finalUrl = base + redirect;
+      if (searchParams) {
+        finalUrl = addSearchParamsToUrl(base, searchParams);
+      }
+      if (clients.openWindow) return clients.openWindow(finalUrl);
+      return;
+    })
+  );
 });
